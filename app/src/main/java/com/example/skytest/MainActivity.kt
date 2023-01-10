340497f9766ac57cba65dc2b3b27e0f783532812
Jose Fadrique Molina Gutierrez <josefadrique.molina@globant.com>
2023-01-09 20:34:08 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/skytest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.skytest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
